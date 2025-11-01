69f08d4678dc234b9cd261ccdfb2bb159c86dd01
PRAKHAR SRIVASTAVA <prasri510@gmail.com>
2025-11-01 21:03:58 +0530

    Setup chat memory (#1)
    * Added chat memory
    
    * added comment

--- src/main/java/com/example/aiagent/controllers/PersonController.java
+++ src/main/java/com/example/aiagent/controllers/PersonController.java
@@ -2,18 +2,21 @@ package com.example.aiagent.controllers;
 
 import com.example.aiagent.models.Person;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/persons")
@@ -22,16 +25,15 @@ public class PersonController {
     private final ChatClient chatClient;
 
     @Autowired
-    private ChatMemoryRepository chatMemoryRepo;
-
-    private final ChatMemory chatMemory = MessageWindowChatMemory
-            .builder()
-            .chatMemoryRepository(chatMemoryRepo)
-            .maxMessages(10)
-            .build();
-
-    public PersonController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    public PersonController(ChatClient.Builder chatClientBuilder, ChatMemoryRepository chatMemoryRepo) {
+        ChatMemory chatMemory = MessageWindowChatMemory
+                .builder()
+                .chatMemoryRepository(chatMemoryRepo)
+                .maxMessages(10)
+                .build();
+        this.chatClient = chatClientBuilder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
     }
 
     // Prompting and structured output
@@ -49,6 +51,17 @@ public class PersonController {
     }
 
     // Advisors and Chat Memory
+    // its picking from in memory chat
+    @GetMapping("/{id}")
+    Person findById(@PathVariable String id) {
+        PromptTemplate pt = new PromptTemplate("""
+                Find and return the object with id {id} in a current list of persons.
+                """);
+        Prompt p = pt.create(Map.of("id", id));
+        return this.chatClient.prompt(p)
+                .call()
+                .entity(Person.class);
+    }
 
 
 
